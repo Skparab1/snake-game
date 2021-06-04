be97113437a6151b0a7c15aeaf75afaacd624690
Shubham <71990977+Skparab1@users.noreply.github.com>
2021-06-03 20:42:10 -0700

    Add files via upload

---
+++ snake game.py
@@ -0,0 +1,22 @@
+#snake game
+import time
+def clearscreen():
+    print('\n'*50)
+clearscreen()
+name = input('enter your name')
+def p(line):
+    print('|'+line+'|')
+def printarena():
+    global l1,l2,l3,l4,l5,l6,l7,l8,l9,l10,l11,l12,l13,l14,l15,l16,l17,l18
+    print('_'*100)
+    p(l1),p(l2),p(l3),p(l4),p(l5),p(l6),p(l7),p(l8),p(l9),p(l10),p(l11),p(l12),p(l13),p(l14),p(l15),p(l16),p(l17),p(l18)
+    print('_'*100)
+l1,l2,l3,l4,l5,l6,l7,l8,l9,l10,l11,l12,l13,l14,l15,l16,l17,l18 = ' '*99,' '*99,' '*99,' '*99,' '*99,' '*99,' '*99,' '*99,' '*99,' '*99,((' '*4)+'ooooo'+(' '*90)),' '*99,' '*99,' '*99,' '*99,' '*99,' '*99,' '*99
+direction = 'right'
+while True:
+    time
+    printarena()
+    lanein = 
+    if direction == 'right':
+
+    
